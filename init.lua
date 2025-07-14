e6cd9c164d348f600e461a531b4a6b3675375633
Sam Huynh <sam.huynh@lyka.com.au>
2025-07-14 14:21:48 +1000

    feat: add catpuccin color scheme

--- init.lua
+++ init.lua
@@ -898,6 +898,22 @@ require('lazy').setup({
     end,
   },
 
+  -- Catppuccin theme
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+    -- Load the colorscheme here.
+    -- Like many other themes, this one has different styles, and you could load
+    -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    vim.cmd.colorscheme 'catppuccin-mocha'
+
+    -- You can configure highlights by doing something like:
+    vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
